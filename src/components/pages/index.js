ac68091ffb0f2352730b8249662ae1ad43e23d29
MYefimenko <marinka0410@gmail.com>
2022-11-30 10:38:05 +0100

    Updates

--- src/components/pages/index.js
+++
@@ -1,6 +0,0 @@
-import MainPage from "./MainPage";
-import SinglePage from "./SinglePage";
-import ComicsPage from "./ComicsPage";
-import Page404 from "./404";
-
-export {MainPage, ComicsPage, Page404, SinglePage};
