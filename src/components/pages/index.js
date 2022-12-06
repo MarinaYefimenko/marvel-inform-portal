13a4d1b5520f3ccb3c639efd31bcbe471f41d3ed
MYefimenko <107976451+MarinaYefimenko@users.noreply.github.com>
2022-12-06 13:03:57 +0100

    add sources files

---
+++ src/components/pages/index.js
@@ -0,0 +1,6 @@
+import MainPage from "./MainPage";
+import SinglePage from "./SinglePage";
+import ComicsPage from "./ComicsPage";
+import Page404 from "./404";
+
+export {MainPage, ComicsPage, Page404, SinglePage};
